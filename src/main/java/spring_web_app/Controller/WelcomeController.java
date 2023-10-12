ab53eb9448d3325c89afa12589953b4f481fd182
pancham01 <pancham9557@gmail.com>
2023-10-12 22:37:33 +0530

    Initial commit

---
+++ src/main/java/spring_web_app/Controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package spring_web_app.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+
+	@RequestMapping("/")
+	public String message()
+	{
+		return "index";
+	}
+}
