30de3225c8f7eb2ebfefb3e7d96e750e3faf8ebd
pancham01 <pancham9557@gmail.com>
2023-12-23 14:56:22 +0530

    update

--- src/main/java/spring_web_app/Controller/WelcomeController.java
+++ src/main/java/spring_web_app/Controller/WelcomeController.java
@@ -2,10 +2,12 @@ package spring_web_app.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WelcomeController {
-
+	
+	@ResponseBody
 	@RequestMapping("/")
 	public String message()
 	{
