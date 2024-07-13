341a62889bec096e5e0863400a7cfa94f7351c5c
pancham01 <pancham9557@gmail.com>
2024-07-13 14:12:09 +0530

    update

--- src/main/java/spring_web_app/Controller/HelloWorldController.java
+++ src/main/java/spring_web_app/Controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package spring_web_app.Controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +9,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
+	@RequestMapping("/login")
+	public String display(HttpServletRequest req, Model m) {
+		
+		String name = req.getParameter("name");
+		String pass = req.getParameter("pass");
+		
+		System.out.println("HelloWorldController.display()");	
+		if (name.equals("kunal") && pass.equals("admin")) {
+			String msg = "HELLO " + name + " Now ALL YOURS IN MVC";
+			// add a message to the model
+			m.addAttribute("message", msg);
+			return "viewPage";
+		} else {
+			String msg = "Sorry " + name + ". You entered an incorrect name or password";
+			m.addAttribute("message", msg);
+			return "errorPage";
+		}
+	}
+
 	@RequestMapping("/login")
 	public String display(@RequestParam(name = "name") String name, @RequestParam(name = "pass") String pass, Model m) {
 		System.out.println("HelloWorldController.display()");	
